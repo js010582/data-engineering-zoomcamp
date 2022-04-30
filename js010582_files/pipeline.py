ac699568db854e8b37738a9e7859c71399db85bb
js010582 <js010582@gmail.com>
2022-04-30 09:07:36 -0700

    adding files

---
+++ js010582_files/pipeline.py
@@ -0,0 +1,11 @@
+import sys
+
+import pandas as pd
+
+print(sys.argv)
+
+day = sys.argv[1]
+
+# fancy stuff with pandas
+
+print ('job finished successfully for day = f{day}')
---
+++ js010582_files/test.py
@@ -0,0 +1,8 @@
+
+n = int(input("Enter n: "))
+
+nums = []
+for _ in range(n):
+    nums.append(int(input()))
+
+print(sum(nums))
